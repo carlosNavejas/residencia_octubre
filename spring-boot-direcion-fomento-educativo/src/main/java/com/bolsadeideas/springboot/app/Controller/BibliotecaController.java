dbf4c0d4e88bf9f805641c32e196d307bd785a58
carlosdircio <carlangasdircionava@gmail.com>
2019-11-02 00:22:46 -0600

    modulo 3 gestion de bibliotecas terminado, falta validar modulo1 y 2 y editar el 1 select

--- spring-boot-direcion-fomento-educativo/src/main/java/com/bolsadeideas/springboot/app/Controller/BibliotecaController.java
+++ spring-boot-direcion-fomento-educativo/src/main/java/com/bolsadeideas/springboot/app/Controller/BibliotecaController.java
@@ -1,9 +1,5 @@
 package com.bolsadeideas.springboot.app.Controller;
 
-import java.util.Map;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,17 +18,25 @@ import com.bolsadeideas.springboot.app.models.entity.Escuela;
 import com.bolsadeideas.springboot.app.models.service.ServiciosServiceImpl;
 
 @Controller
+@SessionAttributes("biblioteca")
 @RequestMapping(value = "/bibliotecas")
 public class BibliotecaController {
 
 	@Autowired
 	private ServiciosServiceImpl servicios;
 
-	@RequestMapping(value = "/form")
-	public String crear(Map<String, Object> model) {
+	@RequestMapping(value = "/form/{id}")
+	public String crear(@PathVariable(value = "id") Long id, Model model, RedirectAttributes flash) {
 		Biblioteca biblioteca = new Biblioteca();
-		model.put("biblioteca", biblioteca);
-		model.put("titulo", "Formulario de bibliotecas");
+		Escuela escuela = servicios.findbyIdEscuela(id);
+		if (escuela == null || id <= 0) {
+			flash.addFlashAttribute("error", "Ha ocurrido un error, intentelo mas tarde");
+			return "redirect:/home";
+		}
+		biblioteca.setEscuela(escuela);
+		model.addAttribute("biblioteca", biblioteca);
+		model.addAttribute("escuela", escuela);
+		model.addAttribute("titulo", "Formulario de bibliotecas");
 		return "Bibliotecas/registarBiblioteca";
 	}
 
@@ -41,7 +46,7 @@ public class BibliotecaController {
 			Biblioteca biblioteca = servicios.findByIdBiblioteca(id);
 			if (biblioteca != null) {
 				servicios.deleteBibliotecaByID(id);
-				flash.addFlashAttribute("success", "Biblioteca eliminada con éxito!");
+				flash.addFlashAttribute("success", "Biblioteca eliminada con éxito!" + biblioteca.toString());
 				return "redirect:/bibliotecas/biblioteca/" + biblioteca.getEscuela().getId_escuela();
 			}
 		}
@@ -50,33 +55,58 @@ public class BibliotecaController {
 	}
 
 	@RequestMapping(value = "/guardar", method = RequestMethod.POST)
-	public String guardar(@Valid Biblioteca biblioteca, BindingResult result, Model model,
+	public String guardar(Biblioteca biblioteca, BindingResult result, Model model,
 			@RequestParam(name = "id_escuela", required = false) Long id_escuela, RedirectAttributes flash,
 			SessionStatus status) {
+		if (biblioteca.getClave_biblioteca() != null) {
+			servicios.saveBiblioteca(biblioteca);
+			status.setComplete();
+			flash.addFlashAttribute("success", "Se ha editado la biblioteca");
 
+			return "redirect:/bibliotecas/biblioteca/" + id_escuela;
+		}
 		Escuela escuela = servicios.findbyIdEscuela(id_escuela);
 		biblioteca.setEscuela(escuela);
 		servicios.saveBiblioteca(biblioteca);
-		flash.addFlashAttribute("success", "Se ha guardado la biblioteca");
-		return "redirect:/bibliotecas/biblioteca/" + escuela.getId_escuela();
+		status.setComplete();
+		flash.addFlashAttribute("success", "Se ha registrado la biblioteca");
+		return "redirect:/bibliotecas/biblioteca/" + id_escuela;
 	}
 
 	@RequestMapping(value = "/biblioteca/{id}")
 	public String verDetallesdeLaCooperativa(@PathVariable(value = "id") Long id, Model model,
 			RedirectAttributes flash) {
+
 		Escuela escuela = null;
 		if (id > 0) {
 			escuela = servicios.findbyIdEscuela(id);
-			if (escuela == null) {
-				flash.addFlashAttribute("error", "Ha ocurrido un error!");
+
+			if (escuela != null) {
+				model.addAttribute("escuela", escuela);
+				model.addAttribute("titulo", "Detalles de la biblioteca");
 				return "/bibliotecas/ver";
-				// return "redirect:/cooperativas/cooperativa/" + id;
+
 			}
 		}
-
-		model.addAttribute("escuela", escuela);
-		model.addAttribute("titulo", "Editar Cooperativa");
-		return "/bibliotecas/ver";
+		flash.addFlashAttribute("error", "Ha ocurrido un error!!");
+		return "redirect:/home";
 	}
 
+	@RequestMapping(value = "/registarBiblioteca/{id}")
+	public String formularioRegistroNuevo(@PathVariable(value = "id") Long id, Model model, RedirectAttributes flash) {
+		Escuela escuela = null;
+		if (id > 0) {
+			escuela = servicios.findbyIdEscuela(id);
+			if (escuela != null) {
+				Biblioteca biblioteca = escuela.getBiblioteca();
+				model.addAttribute("escuela", escuela);
+				model.addAttribute("biblioteca", biblioteca);
+				model.addAttribute("titulo", "Editar Biblioteca");
+				return "Bibliotecas/editarBiblioteca";
+			}
+		}
+		flash.addFlashAttribute("error", "Ha ocurrido un error al editar la cooperativa");
+		model.addAttribute("titulo", "Inicio");
+		return "redirect:/home";
+	}
 }
--- spring-boot-direcion-fomento-educativo/src/main/java/com/bolsadeideas/springboot/app/models/entity/Biblioteca.java
+++ spring-boot-direcion-fomento-educativo/src/main/java/com/bolsadeideas/springboot/app/models/entity/Biblioteca.java
@@ -1,6 +1,8 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -34,44 +36,80 @@ public class Biblioteca implements Serializable {
 	private Date fecha_registro;
 	@OneToOne(fetch = FetchType.LAZY)
 	private Escuela escuela;
+
 	public Long getClave_biblioteca() {
 		return clave_biblioteca;
 	}
+
 	public void setClave_biblioteca(Long clave_biblioteca) {
 		this.clave_biblioteca = clave_biblioteca;
 	}
+
 	public String getNombre_bibioteca() {
 		return nombre_bibioteca;
 	}
+
 	public void setNombre_bibioteca(String nombre_bibioteca) {
 		this.nombre_bibioteca = nombre_bibioteca;
 	}
+
 	public Date getFecha_registro() {
 		return fecha_registro;
 	}
+
 	public void setFecha_registro(Date fecha_registro) {
 		this.fecha_registro = fecha_registro;
 	}
+
 	public Escuela getEscuela() {
 		return escuela;
 	}
+
 	public void setEscuela(Escuela escuela) {
 		this.escuela = escuela;
 	}
+
 	public Biblioteca(Long clave_biblioteca, String nombre_bibioteca, Date fecha_registro, Escuela escuela) {
-		
+
 		this.clave_biblioteca = clave_biblioteca;
 		this.nombre_bibioteca = nombre_bibioteca;
 		this.fecha_registro = fecha_registro;
 		this.escuela = escuela;
 	}
+
 	public Biblioteca() {
-		
+
 	}
+
 	@PrePersist
 	public void prePersist() {
 		fecha_registro = new Date();
 	}
-	
-	
+
+	public String obtenerPeriodoEscolar() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(fecha_registro);
+		int mes = calendar.get(Calendar.MONTH);
+
+		if (mes >= 7) {
+			return "Agosto-Diciembre";
+		}
+		return "Enero-Junio";
+	}
+
+	public String obtenerCicloEscolar() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(fecha_registro);
+		int inicio = calendar.get(Calendar.YEAR);
+		int fin = inicio + 1;
+
+		return inicio + "-" + fin;
+	}
+
+	@Override
+	public String toString() {
+		return "Biblioteca [clave_biblioteca=" + clave_biblioteca + ", nombre_bibioteca=" + nombre_bibioteca
+				+ ", fecha_registro=" + fecha_registro + ", escuela=" + escuela + "]";
+	}
+
 }
